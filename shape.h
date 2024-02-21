79f2288b3eff980b508a278fee5a64820f7b0e39
sm3232 <connor.hill@mymail.champlain.edu>
2024-02-20 21:30:30 -0500

    Added shape.h struc

---
+++ shape.h
@@ -0,0 +1,30 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include "point.h"
+#include "color.h"
+#include <vector>
+
+struct Shape {
+  Point position;
+  Point velocity;
+  Point force;
+  float torque;
+  float rotation;
+  float angularVelocity;
+  Color color;
+  std::vector<Point> verts;
+  Shape(
+    const Point &position_,
+    const Color &color_,
+    const Point &initialVelocity = Point(0, 0),
+    const float &initialRotation = 0,
+    const Point &initialForce = Point(0, 0),
+    const float &initialAngularVelocity = 0,
+    const float &initialTorque = 0
+  ) : position(position_), color(color_), velocity(initialVelocity),
+      rotation(initialRotation), force(initialForce), angularVelocity(initialAngularVelocity),
+      torque(initialTorque){};
+};
+
+#endif
